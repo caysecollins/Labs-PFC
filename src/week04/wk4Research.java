f44fe471cd233106e4821b9aa428463f58b6d003
CayseCollins <codecravt@gmail.com>
2023-07-28 20:36:47 -0600

    adding wk4 Lab

---
+++ src/week04/wk4Research.java
@@ -0,0 +1,42 @@
+package week04;
+
+public class wk4Research {
+
+	public static void main(String[] args) {
+/* Prompts
+
+3. What are the differences between Lists, Sets, and Maps in Java?
+
+A: 
+
+Sources:
+
+4. List at least two different implementations for each collection (List, Set, and Map). 
+When would you use one of the implementations over the other?
+
+A:
+
+Sources:
+
+*/
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
